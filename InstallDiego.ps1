a2d59d2d3e6c8e4feb39c0a1aa978cc3a1ef0584
Martin Kreibe <mkreibe@users.noreply.github.com>
2016-07-27 14:14:41 -0700

    Fixed various issues.
    * Fixed issue with Zip (No longer required Powershell 5)
    * Now checks for execution policy
    * Cleaned up labels
    * BOSH user is now defaulted
    * IP Addresses are listed and generating an assumed BOSH server IP
    * Fixed issue where the script needed to run in the folder that was being generated.
    * Fixed pathing with install.bat
    * Added debugging around the bat file execution
    * Added parameters to the script
    * Added help to the script

--- InstallDiego.ps1
+++ InstallDiego.ps1
@@ -1,21 +1,27 @@
 # Install Diego onto a windows machine script.
-$diego_version = '1.7.7'
-$debug = $false
-$do_cleanup = $true
+Param (
+    [string] $diego,
+    [switch] $debug,
+    [switch] $dirty,
+    [switch] $help,
+    [switch] $usedefaults,
+    [string] $user,
+    [string] $password,
+    [string] $ip,
+    [int] $port
+)
 
 Clear-Host
 
 $pivotal_url = 'https://network.pivotal.io/products/elastic-runtime'
 $unzip_location = 'C:\diego-installs'
-$user = [Environment]::UserName;
-$zip_file = "C:\Users\$($user)\Downloads\DiegoWindows$($diego_version).zip"
 $zip_structure = 'DiegoWindows'
-$port = 25555
 
 $input_color = 'Green'
-$info_color = 'Gray'
+$info_color = 'DarkGray'
+$error_color = 'Red'
 $action_color = 'DarkYellow'
-$debug_color = 'Red'
+$debug_color = 'Magenta'
 
 # Show setup.
 Write-Host "
@@ -25,86 +31,217 @@ Write-Host "
 |____/|_|___|_  |___|  |_____|_|_|  |_____|_|_|_|___|___|_____|___|
             |___|               
 "
-if($debug -or -not $do_cleanup) {
-    Write-Host "   Flags [Debug: $debug] [Cleanup: $do_cleanup] `n" -ForegroundColor $debug_color
+
+if($help) {
+    Write-Host ".\InstallDiego.ps1 "
+    Write-Host "     [-diego <DIEGO_VERSION>]"
+    Write-Host "     [-debug]"
+    Write-Host "     [-dirty]"
+    Write-Host "     [-help]"
+    Write-Host "     [-usedefaults]"
+    Write-Host "     [-user <BOSH Director Administrator Name>]"
+    Write-Host "     [-password <BOSH Director Administrator Password>]"
+    Write-Host "     [-ip <BOSH Director IP Address>]"
+    Write-Host "     [-port <BOSH Director Port>]"
+    Write-Host "`n"
+    exit(0);
 }
+
+# Run the rest of the script.
+if($debug -or $dirty -or $usedefaults) {
+    Write-Host "   Flags [Debug: $debug] [Cleanup: $(-not $dirty)] [Use Defaults: $usedefaults]`n" -ForegroundColor $debug_color
+}
+
+Write-Host "Checking overrides..." -ForegroundColor $info_color 
+
+if($diego -eq $null -or $diego -eq "") {
+    $diego = '1.7.7'
+} else {
+    Write-Host "   Diego Version: $diego" -ForegroundColor $info_color
+}
+
+# Check the port
+if($port -eq 0) {
+    $port = 25555
+} else {
+    if($port -gt 0 -and $port -lt 65535) {
+        Write-Host "   Port: $port" -ForegroundColor $info_color
+    } else {
+        $shell = New-Object -ComObject Wscript.Shell
+        $shell.Popup("Port must be between 0 and 65535.", 5, "Invalid port", 0x30)
+        Write-Host "Port must be between 0 and 65535 [value: $port]." -ForegroundColor $error_color
+        exit(-1)
+    }
+}
+
+if($user -eq "") {
+    $user = 'director'
+} else {
+    Write-Host "   User: $user" -ForegroundColor $info_color
+}
+
+if($password -ne "") {
+    Write-Host "   Password: ********" -ForegroundColor $info_color
+}
+
+if($ip -ne "") {
+    Write-Host "   IP Address: $ip" -ForegroundColor $info_color
+}
+
+# Write the diego version.
+Write-Host "Assumed Diego [Version: $diego]" -ForegroundColor $info_color
 
 # Check powershell version.
 $psver = $PSVersionTable.PSVersion;
 Write-Host "Powershell [Version: $psver]" -ForegroundColor $info_color
 if($psver.Major -lt 4) {
-  $shell = New-Object -ComObject Wscript.Shell
-  $shell.Popup("You must be running Powershell version 4 or greater", 5, "Invalid Powershell version", 0x30)
-  Write-Host "You must be running Powershell version 4 or greater"
-  exit(-1)
+    $shell = New-Object -ComObject Wscript.Shell
+    $shell.Popup("You must be running Powershell version 4 or greater", 5, "Invalid Powershell version", 0x30)
+    Write-Host "You must be running Powershell version 4 or greater" -ForegroundColor $error_color
+    exit(-1)
+}
+
+$policy = Get-ExecutionPolicy
+if($policy -ne "Unrestricted") {
+    $shell = New-Object -ComObject Wscript.Shell
+    $shell.Popup("The execution policy must be unrestricted!`nCurrent policy: $($policy)", 5, "Invalid execution policy", 0x30)
+    Write-Host "The execution policy must be unrestricted!" -ForegroundColor $error_color
+    Write-Host "Run: `"Set-ExecutionPolicy Unrestricted -Scope CurrentUser`"" -ForegroundColor $info_color
+    exit(-1)
 }
 
 #Check OS.
 $osver = [System.Environment]::OSVersion.Version
 Write-Host "Operating System [Version: $osver] [Platform: $([System.Environment]::OSVersion.Platform)]" -ForegroundColor $info_color
 
+#Check IP address
+Write-Host "IP Addresses:" -ForegroundColor $info_color
+$assume_bosh = $false
+foreach($item in Get-NetIPAddress) {
+    if($ip -eq "" -and $item.AddressFamily -eq "IPv4") {
+        $ip = "$($item.IPAddress.Substring(0, $item.IPAddress.LastIndexOf('.'))).50"
+        $assume_bosh = $true
+    }
+    Write-Host "`t$($item.IPAddress)" -ForegroundColor $info_color
+}
+
+if($assume_bosh) {
+    Write-Host "Assumed BOSH Director IP Address: $ip" -ForegroundColor $info_color
+}
+
 # Get the zip and put it somewhere.
 # TODO: in the future we may want to add a
 #         Invoke-WebRequest $pivotal_url -OutFile $zip_staging
 #       type of construct that would auto download the file.
-Write-Host "`nDownload the zip from: $pivotal_url" -ForegroundColor $action_color
-Write-Host "Enter the location of the zip file [$($zip_file)]: " -NoNewline -ForegroundColor $input_color
-$prompt = Read-Host
-if (!$prompt -eq "") { $zip_file = $prompt }
+$zip_file = "C:\Users\$([Environment]::UserName)\Downloads\DiegoWindows$($diego).zip"
+if(-not $usedefaults) {
+    Write-Host "`nDownload the zip from: $pivotal_url" -ForegroundColor $action_color
+    Write-Host "Enter the location of the zip file [$($zip_file)]: " -NoNewline -ForegroundColor $input_color
+    $prompt = Read-Host
+    if (!$prompt -eq "") { $zip_file = $prompt }
+}
 
 if(Test-Path $unzip_location) {
-    Write-Host "Removing remnants for unzip location." -ForegroundColor $info_color
+    Write-Host "Removing remnants for unzip location ($unzip_location)." -ForegroundColor $info_color
     Remove-Item -path $unzip_location -Force -Recurse -Confirm:$false
 }
 
 Write-Host "Unzipping $($unzip_location) ..." -ForegroundColor $info_color
-Expand-Archive $zip_file -Dest $unzip_location
+if($psver.Major -ge 5) {
+    Expand-Archive $zip_file -Dest $unzip_location
+} else {
+    New-Item $unzip_location -type directory -Force | Out-Null
+    $shell = New-Object -com Shell.Application
+    $zip = $shell.Namespace($zip_file)
+    foreach($item in $zip.items()) {
+        $shell.Namespace($unzip_location).copyhere($item)
+    }
+}
+
+# Move to the unzip location.
+$orig_folder = Get-Location
+Write-Host "Changing Folders: [Old: $($orig_folder)] [New: $($unzip_location)] ..." -ForegroundColor $info_color
+Set-Location -Path $unzip_location
 
 # Extract the file.
 $setup_file_location = "$unzip_location\$zip_structure\setup.ps1"
 Write-Host "Executing $($setup_file_location) ..." -ForegroundColor $info_color
 if(-not $debug) {
-    Invoke-Expression "$setup_file_location"
+    Unblock-File -Path "$setup_file_location" -Confirm:$false
+    Invoke-Expression "$setup_file_location -quiet"
 } else {
     Write-Host "   Debugging, Skipping." -ForegroundColor $debug_color
 }
 
 # Call the generator.
-Write-Host "Enter the user name [$($user)]: " -NoNewline -ForegroundColor $input_color
-$prompt = Read-Host
-if (!$prompt -eq "") { $user = $prompt }
+if(-not $usedefaults) {
+    Write-Host "Enter the BOSH Administrator [$($user)]: " -NoNewline -ForegroundColor $input_color
+    $prompt = Read-Host
+    if (!$prompt -eq "") { $user = $prompt }
+}
 
-$password = Read-Host 'Enter the users password' -AsSecureString
-Write-Host "Password captured" -ForegroundColor $input_color
+if(-not $usedefaults -and $password -ne "") {
+    Write-Host 'Enter the BOSH Administrator password [********]: ' -NoNewline -ForegroundColor $input_color
+    $prompt = Read-Host
+    if (!$prompt -eq "") { $password = $prompt }
+} elseif($password -eq "") {
+    Write-Host 'Enter the BOSH Administrator password: ' -NoNewline -ForegroundColor $input_color
+    $password = Read-Host
+}
 
-Write-Host 'Enter the BOSH Director URL: ' -NoNewline -ForegroundColor $input_color
-$url = Read-Host
+if(-not $usedefaults) {
+    Write-Host "Enter the BOSH Director Machine IP address [$($ip)]: " -NoNewline -ForegroundColor $input_color
+    $prompt = Read-Host
+    if (!$prompt -eq "") { $ip = $prompt }
+}
 
-Write-Host "Enter the port [$($port)]: " -NoNewline -ForegroundColor $input_color
-$prompt = Read-Host
-if (!$prompt -eq "") { $port = $prompt }
+#TODO: ping the director machine.
+
+if(-not $usedefaults) {
+    Write-Host "Enter the BOSH Director Machine port [$($port)]: " -NoNewline -ForegroundColor $input_color
+    $prompt = Read-Host
+    if (!$prompt -eq "") { $port = $prompt }
+}
 
 $generate_file_location = "$unzip_location\$zip_structure\generate.exe"
 
-Write-Host "Executing $generate_file_location --boshUrl https://$($user):********@$($url):$($port) -outputDir $unzip_location ..." -ForegroundColor $info_color
+Write-Host "Executing $generate_file_location --boshUrl https://$($user):********@$($ip):$($port) -outputDir $unzip_location ..." -ForegroundColor $info_color
 if(-not $debug) {
-    $unsecure = $password | ConvertFrom-SecureString
-    Invoke-Expression "$generate_file_location --boshUrl https://$($user):$($unsecure)@$($url):$($port) -outputDir $unzip_location"
+    Invoke-Expression "$generate_file_location --boshUrl https://$($user):$($password)@$($ip):$($port) -outputDir $unzip_location"
 } else {
     Write-Host "   Debugging, Skipping." -ForegroundColor $debug_color
 }
 
-# Invoke the install 
-$install_file_location = "$unzip_location\$zip_structure\install.bat"
-Write-Host "Invoking $install_file_location ..." -ForegroundColor $info_color
-if(-not $debug) {
-    Invoke-Expression "$install_file_location"
+# Invoke the install
+$install_file_location = "$unzip_location\install.bat"
+Write-Host "Looking for $install_file_location ..." -ForegroundColor $info_color
+if(Test-Path $install_file_location) {
+    Write-Host "Invoking $install_file_location ..." -ForegroundColor $info_color
+    if(-not $debug) {
+        $batch = Start-Process -FilePath $install_file_location -Wait -RedirectStandardError install_log.txt -WorkingDirectory "$unzip_location" #\$zip_structure"
+
+        # write the error code, error message and batch file on errors.
+        if($batch.ExitCode -ne $null) {
+            Write-Host "   Exit Code: $($batch.ExitCode)" -ForegroundColor $error_color
+            $batch = NET HELPMSG $($batch.ExitCode)
+            Write-Host "   Network Error: $($batch)" -ForegroundColor $error_color
+            $file_contents = TYPE install_log.txt
+            Write-Host "   Logs:`n$file_contents" -ForegroundColor $info_color
+        } else {
+            Write-Host "   Batch script completed successfully" -ForegroundColor $info_color
+        }
+    } else {
+        Write-Host "   Debugging, Skipping." -ForegroundColor $debug_color
+    }
 } else {
-    Write-Host "   Debugging, Skipping." -ForegroundColor $debug_color
+    Write-Host "Something went wrong! There is no '$install_file_location' file." -ForegroundColor $error_color
 }
 
 # Cleanup!
-if($do_cleanup) {
+Write-Host "Changing Folders back to the original [$($orig_folder)] ..." -ForegroundColor $info_color
+Set-Location -Path $orig_folder
+
+if(-not $dirty) {
     Write-Host "Cleaning up." -ForegroundColor $info_color
     if(Test-Path $unzip_location) {
         Remove-Item -path $unzip_location -Force -Recurse -Confirm:$false
